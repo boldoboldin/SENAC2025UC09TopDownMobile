8fdaf1883c09e0b63a5e0a38052ee3433e667457
Henrique Baldo Carlos <henriquebaldocarlos@gmail.com>
2025-02-28 16:38:16 -0300

    MineDrone states

---
+++ Assets/Scripts/Enemies/MineDrone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineDrone : EnemyCtrl
+{
+    [SerializeField] private float explosionDistance;
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distance < explosionDistance)
+        {
+            ActivateAtk();
+        }
+    }
+
+    void ActivateAtk()
+    {
+        anim.SetTrigger("Activate");
+    }
+
+    void Explode()
+    {
+        Vector2 instatePos = new Vector2(transform.position.x, transform.position.y + 1f);
+
+        GameObject fx = Instantiate (sExplosionFX, instatePos, transform.rotation);
+    }
+}
